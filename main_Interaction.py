b5dfc3c6ac4d46dd36ebacb4597edf780f020080
WeianJiang <tbszjwa@outlook.com>
2019-05-26 19:29:17 +0800

    Update main_interaction.py

--- main_Interaction.py
+++ main_Interaction.py
@@ -2,6 +2,19 @@ from abaqus import *
 from abaqusConstants import *
 
 
+def creatingTie(referencePointIndex,instanceName,end):
+    a = mdb.models['Model-1'].rootAssembly
+    r1 = a.referencePoints
+    refPoints1=(r1[referencePointIndex], )
+    region1=a.Set(referencePoints=refPoints1, name='m_Set-1')
+    a = mdb.models['Model-1'].rootAssembly
+    v1 = a.instances[instanceName].vertices
+    verts1 = v1.getSequenceFromMask(mask=('[#'+str(end)+']', ), )
+    region2=a.Set(vertices=verts1, name='s_Set-1')
+    mdb.models['Model-1'].Tie(name='Constraint-1', master=region1, slave=region2, 
+        positionToleranceMethod=COMPUTED, adjust=ON, tieRotations=ON, thickness=ON)
+
+
 
 def creatingTie2(referencePointIndex,instanceName1,end1,instanceName2,end2):
     a = mdb.models['Model-1'].rootAssembly
@@ -16,7 +29,6 @@ def creatingTie2(referencePointIndex,instanceName1,end1,instanceName2,end2):
     region2=a.Set(vertices=verts1+verts2, name='s_Set-1')
     mdb.models['Model-1'].Tie(name='Constraint-1', master=region1, slave=region2, 
         positionToleranceMethod=COMPUTED, adjust=ON, tieRotations=ON, thickness=ON)
-
 
 
 def creatingTie3(referencePointIndex,instanceName1,end1,instanceName2,end2,instanceName3,end3):
