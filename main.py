eab33c279b5cf0428a1b64e974a7902b47764c7e
WeianJiang <tbszjwa@outlook.com>
2019-05-28 19:43:20 +0800

    update

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def callFunction(Func,looptimes):
         Func(partName[number])
 
         
-circleData=np.loadtxt('Circle.txt')
+# circleData=np.loadtxt('Circle.txt')
 
-length=5
+length=10
 
 
 eleNum=2*length*(length+1)
@@ -33,37 +33,37 @@ for number in range(eleNum):
 #---------------------------------------------------------------------------------------------------Part Process
 map(main_PartGen.partGen,partName)
 #--------------------------------------------------------------------------------------------------Property Process
-elasticMod=[]
-possionRat=[]
-main_Property.profileCreate('profile-1',0.1,0.1)
-for number in range(eleNum):
-        elasticMod.append(60000)
-        possionRat.append(0.2)
+# elasticMod=[]
+# possionRat=[]
+# main_Property.profileCreate('profile-1',0.1,0.1)
+# for number in range(eleNum):
+#         elasticMod.append(60000)
+#         possionRat.append(0.2)
 
-pointNOTIncircle=pointDeterminator.pointDeterminator(pointcode,circleData)
-for number in range(eleNum):
-        for numberofpointNOtinCircle in range(len(pointNOTIncircle)):#giving different para to the element
-                if number==pointNOTIncircle[numberofpointNOtinCircle][2]:
-                        elasticMod[number]=21000
-                        possionRat[number]=0.2
-for number in range(eleNum):
-        main_Property.materialCreate(partName[number],elasticMod[number],possionRat[number])
-        main_Property.sectionCreate(partName[number],'profile-1',partName[number])
-map(main_Property.assignBeamDirect,partName)
-map(main_Property.assignSection,partName,partName)
+# pointNOTIncircle=pointDeterminator.pointDeterminator(pointcode,circleData)
+# for number in range(eleNum):
+#         for numberofpointNOtinCircle in range(len(pointNOTIncircle)):#giving different para to the element
+#                 if number==pointNOTIncircle[numberofpointNOtinCircle][2]:
+#                         elasticMod[number]=21000
+#                         possionRat[number]=0.2
+# for number in range(eleNum):
+#         main_Property.materialCreate(partName[number],elasticMod[number],possionRat[number])
+#         main_Property.sectionCreate(partName[number],'profile-1',partName[number])
+# map(main_Property.assignBeamDirect,partName)
+# map(main_Property.assignSection,partName,partName)
 #-------------------------------------------------------------------------------------------------Assembly Process
 map(main_PartAssem.partInst,partName)
 callFunction(main_PartAssem.partRotate,eleNum/2)#rotate half of the beams
 for number in range(eleNum):
         main_PartAssem.partTranslate('Part-'+str(number),pointcode[number][0],pointcode[number][1])
 #-------------------------------------------------------------------------------------------------create reference point
-RPCoordinate=[]
-for number in range(eleNum/2): 
-        RPIndex=main_PartAssem.referencePointCreate(pointcode[number][0],pointcode[number][1])
-        RPCoordinate.append([pointcode[number][0],pointcode[number][1],RPIndex])
-for number in range(eleNum/2-length-1,eleNum/2): #generate reference point for the toppest row
-        RPIndex=main_PartAssem.referencePointCreate(pointcode[number][0],pointcode[number][1]+1)
-        RPCoordinate.append([pointcode[number][0],pointcode[number][1]+1,RPIndex])
+# RPCoordinate=[]
+# for number in range(eleNum/2): 
+#         RPIndex=main_PartAssem.referencePointCreate(pointcode[number][0],pointcode[number][1])
+#         RPCoordinate.append([pointcode[number][0],pointcode[number][1],RPIndex])
+# for number in range(eleNum/2-length-1,eleNum/2): #generate reference point for the toppest row
+#         RPIndex=main_PartAssem.referencePointCreate(pointcode[number][0],pointcode[number][1]+1)
+#         RPCoordinate.append([pointcode[number][0],pointcode[number][1]+1,RPIndex])
 
 # i=0
 # for number in range(len(RPCoordinate)):#generate referencepoint coordinate and its index
@@ -71,47 +71,47 @@ for number in range(eleNum/2-length-1,eleNum/2): #generate reference point for t
 #         i+=1
 #---------------------------------------------------------------------------------------------Interaction Process
 
-for i in range(len(pointcode)):
-        x=pointcode[i][0]
-        y=pointcode[i][1]
-        RPIndex=ToolKit.findRPIndex(x,y,RPCoordinate)
-        leftIns=ToolKit.findLeftInstance(x,y,length,pointcode)
-        rightIns=ToolKit.findRightInstance(x,y,length,pointcode)
-        upIns=ToolKit.findUpInstance(x,y,length,pointcode)
-        downIns=ToolKit.findDownInstance(x,y,length,pointcode)
-        if leftIns>=0:
-                main_Interaction.creatingTie(RPIndex,'Part-'+str(leftIns),2,i)
-        if rightIns>=0:
-                main_Interaction.creatingTie(RPIndex,'Part-'+str(rightIns),1,i)
-        if upIns>=0:
-                main_Interaction.creatingTie(RPIndex,'Part-'+str(upIns),1,i)
-        if downIns>=0:
-                main_Interaction.creatingTie(RPIndex,'Part-'+str(downIns),2,i)
-RPIndex=ToolKit.findRPIndex(length,length,RPCoordinate)
-leftIns=ToolKit.findLeftInstance(length,length,length,pointcode)
-downIns=ToolKit.findDownInstance(length,length,length,pointcode)
-main_Interaction.creatingTie(RPIndex,'Part-'+str(leftIns),2,'last')
-main_Interaction.creatingTie(RPIndex,'Part-'+str(downIns),2,'last')
-#---------------------------------------------------------------------------step process
-mdb.models['Model-1'].StaticStep(name='Step-1', previous='Initial')
-#---------------------------------------------------------------------------Load process
-highestPointRPIndex=ToolKit.findToppestNode(length,pointcode,RPCoordinate)
-for i in range(len(highestPointRPIndex)):
-        main_Load.setLoad(highestPointRPIndex[i],0.1,i)
-lowestPointRPIndex=ToolKit.findLowestNode(length,pointcode,RPCoordinate)
-for i in range(len(lowestPointRPIndex)):
-        main_Load.setBoundary(lowestPointRPIndex[i],i)
-#----------------------------------------------------------------------------Mesh Process
-for i in range(len(partName)):
-        main_Mesh.Mesh(partName[i])
+# for i in range(len(pointcode)):
+#         x=pointcode[i][0]
+#         y=pointcode[i][1]
+#         RPIndex=ToolKit.findRPIndex(x,y,RPCoordinate)
+#         leftIns=ToolKit.findLeftInstance(x,y,length,pointcode)
+#         rightIns=ToolKit.findRightInstance(x,y,length,pointcode)
+#         upIns=ToolKit.findUpInstance(x,y,length,pointcode)
+#         downIns=ToolKit.findDownInstance(x,y,length,pointcode)
+#         if leftIns>=0:
+#                 main_Interaction.creatingTie(RPIndex,'Part-'+str(leftIns),2,i)
+#         if rightIns>=0:
+#                 main_Interaction.creatingTie(RPIndex,'Part-'+str(rightIns),1,i)
+#         if upIns>=0:
+#                 main_Interaction.creatingTie(RPIndex,'Part-'+str(upIns),1,i)
+#         if downIns>=0:
+#                 main_Interaction.creatingTie(RPIndex,'Part-'+str(downIns),2,i)
+# RPIndex=ToolKit.findRPIndex(length,length,RPCoordinate)
+# leftIns=ToolKit.findLeftInstance(length,length,length,pointcode)
+# downIns=ToolKit.findDownInstance(length,length,length,pointcode)
+# main_Interaction.creatingTie(RPIndex,'Part-'+str(leftIns),2,'last')
+# main_Interaction.creatingTie(RPIndex,'Part-'+str(downIns),2,'last')
+# #---------------------------------------------------------------------------step process
+# mdb.models['Model-1'].StaticStep(name='Step-1', previous='Initial')
+# #---------------------------------------------------------------------------Load process
+# highestPointRPIndex=ToolKit.findToppestNode(length,pointcode,RPCoordinate)
+# for i in range(len(highestPointRPIndex)):
+#         main_Load.setLoad(highestPointRPIndex[i],0.1,i)
+# lowestPointRPIndex=ToolKit.findLowestNode(length,pointcode,RPCoordinate)
+# for i in range(len(lowestPointRPIndex)):
+#         main_Load.setBoundary(lowestPointRPIndex[i],i)
+# #----------------------------------------------------------------------------Mesh Process
+# for i in range(len(partName)):
+#         main_Mesh.Mesh(partName[i])
 
-#print circleData
-# a = mdb.models['Model-1'].rootAssembly
-# a.regenerate()
-# a = mdb.models['Model-1'].rootAssembly
-mdb.Job(name='Job-1', model='Model-1', description='')
-mdb.jobs['Job-1'].writeInput(consistencyChecking=OFF)
-print 'success input'
+# #print circleData
+# # a = mdb.models['Model-1'].rootAssembly
+# # a.regenerate()
+# # a = mdb.models['Model-1'].rootAssembly
+# mdb.Job(name='Job-1', model='Model-1', description='')
+# mdb.jobs['Job-1'].writeInput(consistencyChecking=OFF)
+# print 'success input'
 
 mdb.saveAs(
     pathName='F:/AbaqusWorkDrt/MesoBeamModel/CodeFile/2D-Lattice-Model/test')
